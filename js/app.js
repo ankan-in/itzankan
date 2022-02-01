8aae1555be9e5c8a0b954fe5d0a9249fb86141c0
itzankan <akmdgamer1290@gmail.com>
2022-02-01 15:29:21 +0530

    ANKAN

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//foot icon
 var youtube = document.querySelector('.icon1');
 var insta = document.querySelector('.icon2');
 var github = document.querySelector('.icon3');
@@ -14,6 +15,34 @@ github.addEventListener('click', () => {
 });
 twitter.addEventListener('click', () => {
     window.open("https://twitter.com/ankan_Q_Q","","height=700px,width=1000px;")
+});
+
+var home = document.querySelector('#home');
+home.addEventListener('click', () => {
+
+window.scrollTo(0,0);
+
+
+});
+
+//main icon
+
+var youtube1 = document.querySelector('.soci1');
+var insta1 = document.querySelector('.soci2');
+var github1 = document.querySelector('.soci3');
+var twitter1 = document.querySelector('.soci4');
+
+youtube1.addEventListener('click', () => {
+    window.open("https://www.youtube.com/channel/UChJIxyWb6UKgPGJbKXG_d8Q","")
+});
+insta1.addEventListener('click', () => {
+    window.open("https://www.instagram.com/itzankan","");
+});
+github1.addEventListener('click', () => {
+    window.open("https://www.github.com/ankan-in","");
+});
+twitter1.addEventListener('click', () => {
+    window.open("https://twitter.com/ankan_Q_Q","");
 });
 
 var home = document.querySelector('#home');
